9a7ebf503636a5ee18975b4feac7c1bf411050ad
Dami <68552016+xxkhione@users.noreply.github.com>
2024-08-09 11:14:51 -0600

    Updated the Header.js and Added Nav.js.

--- website/front-end/nature-gambit/src/components/Header.js
+++ website/front-end/nature-gambit/src/components/Header.js
@@ -1,7 +1,11 @@
+import React from 'react'
+import Nav from './Nav'
+
 function Header(){
     return (
         <header className="App-header">
             <p>Nature's Gambit</p>
+            <Nav />
         </header>
     )
 }
---
+++ website/front-end/nature-gambit/src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav(){
+    return (
+        <nav className="navBar">
+            
+        </nav>
+    )
+}
+
+export default Nav;
