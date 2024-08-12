d1fd50940bedabc0051fabe76c52893f397e674f
RedSousuke <69915077+PrinceSousuke@users.noreply.github.com>
2024-08-12 11:26:11 -0600

    adjust uo files

--- intellij project/ApplicationBase/src/main/java/pro100/group6/applicationbase/NGStartController.java
+++ intellij project/ApplicationBase/src/main/java/pro100/group6/applicationbase/NGStartController.java
@@ -1,27 +1,16 @@
 package pro100.group6.applicationbase;
 
-import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.Objects;
-import java.util.ResourceBundle;
-import java.util.Stack;
 
 public class NGStartController{
     @FXML
