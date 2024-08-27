3a528352222fe2c09e737f3f1d53887a0c3c3f4f
Dami <68552016+xxkhione@users.noreply.github.com>
2024-08-27 14:05:20 -0600

    Got rid of the back-end & updated text.

--- website/front-end/nature-gambit/src/pages/AboutUs.js
+++ website/front-end/nature-gambit/src/pages/AboutUs.js
@@ -7,7 +7,20 @@ function AboutUs(){
         <div className="App">
             <Header title="About the Developers!"/>
             <Nav />
-            <p>We're pretty cool developers!</p>
+            <p>Ryan Meeks (Game Developer)</p>
+            <p>I enjoy studying computer science related things, primarily game development and ethical hacking. If I feel good about what I have learned to do so far, I look for something more difficult to learn. When developing an application, I often try to make it work as fluid as I can, sometimes spending too much time.</p>
+            <p>Ben Ludolf (Game Developer)</p>
+            <p>I'm Ben Ludolf, a current student at Neumont College of Computer Science. My life goal is to work remotely and travel. I have many passions including weightlifting, music, and overall just learning new things. A fun fact about me is I can bench two plates or 225 which only 0.075% of humans can do.</p>
+            <p>Amber Oliver (Graphic Designer)</p>
+            <p>
+                Hi my name is Amber Oliver and here's a little  about me, I grew up in a small town in Alabama and went to the local high school. I was in marching band for 7 years and played the flute and piccolo. I now go to the same college as my older brother and live in Utah, which is pretty far from home. 
+                Here's a little fun fact about me as well, many people think I'm from England because of how I say certain words, but it's actually because of a speech impediment.
+            </p>
+            <p>Damia Sargent (Website Developer)</p>
+            <p>
+                I have a deep and growing passion to learn new things and how to adapt to the changing world. I have been programming since the age of 8 (12 years!), which is why I chose Neumont.
+                I have grown a liking to programming websites as well as dealing with data. My life goal is to eventually be good enough at what I do to land a very successful job and grow my knowledge!
+            </p>
         </div>
     )
 }
--- website/front-end/nature-gambit/src/pages/Rules.js
+++ website/front-end/nature-gambit/src/pages/Rules.js
@@ -18,12 +18,14 @@ there are consequences to pay! Your max health will decrease by the amount of Fe
             </p>
             <p className="App-rulesHeaders">Turns</p>
             <p>
-                Placeholder!
+                Player one will go first and upon each turn they draw 3 cards from their deck. (How many cards can be played placeholder). Once a player has no cards in their deck
+                they will take damage upon every turn starting at -1HP and doubling after their turn has ended. This will be the downfall of using cards to swiftly
+                and making unwise decisions.
             </p>
             <p className="App-rulesHeaders">Decks</p>
             <p>
-                Player decks must have 20 cards out of 88 cards to choose from.
-                Placeholder!
+                Player decks must have 20 cards out of 88 cards to choose from. Each deck can only consist of one type (Earth, Water, Fire, and Air) and can have up to 9
+                neutral cards. Make sure to choose your cards wisely as they will determine your fate of the game.
             </p>
             <p className="App-rulesHeaders">Gameplay</p>
             <p>
