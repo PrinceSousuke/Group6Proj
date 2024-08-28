dd0d5755ac21e29d4f1b73a963f3407358e1cc7e
Dami <68552016+xxkhione@users.noreply.github.com>
2024-08-28 10:24:52 -0600

    Final touches to website.

--- website/front-end/nature-gambit/src/components/Window.js
+++ website/front-end/nature-gambit/src/components/Window.js
@@ -3,7 +3,10 @@ import React from 'react'
 function Window(){
     return(
         <div className="Window-container">
-            <div className="App-window"></div>
+            <div className="App-window">
+                <img src="mainMenu.png" alt="main menu example"/>
+                <img src="gameInAction.png" alt="active gameplay"/>
+            </div>
         </div>
     )
 }
